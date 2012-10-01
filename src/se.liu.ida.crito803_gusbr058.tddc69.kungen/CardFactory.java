d533bb3f016912cdc1e3decbb0d8dd41ca06b745
Cristian Torrusio <crito803@zaza9.ida.liu.se>
2012-10-01 17:34:10 +0200

    ny version upcoming mofos!

---
+++ src/se.liu.ida.crito803_gusbr058.tddc69.kungen/CardFactory.java
@@ -0,0 +1,11 @@
+package se.liu.ida.crito803_gusbr058.tddc69.kungen;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: crito803
+ * Date: 2012-10-01
+ * Time: 13:52
+ * To change this template use File | Settings | File Templates.
+ */
+public class CardFactory {
+}
